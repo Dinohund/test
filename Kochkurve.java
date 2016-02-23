8c3dc8b8c7f8743cc90e5a9cfe935f10b67cb4df
Dinohund <tominator96@live.de>
2016-02-23 23:03:41 +0100

    Added files via upload

---
+++ Kochkurve.java
@@ -0,0 +1,26 @@
+public class Kochkurve {
+
+	static void kochkurve(Staffelei s, int ordnung) {
+		if(ordnung == 0){
+			s.drawForward(10);
+		}
+		else{
+			kochkurve(s,ordnung-1);
+			s.rotate(-60);
+			kochkurve(s,ordnung-1);
+			s.rotate(120);
+			kochkurve(s,ordnung-1);
+			s.rotate(-60);
+			kochkurve(s,ordnung-1);
+			}
+	}
+	public static void main(String[] args) {
+		Staffelei s = new Staffelei();
+		s.rotate(-90);
+		kochkurve(s, 3);
+		s.rotate(120);
+		kochkurve(s, 3);
+		s.rotate(120);
+		kochkurve(s, 3);
+	}
+}
